4fb9e93d0db28894959e792e448840482a87d3e2
codedompteur <timo.maiwald@web.de>
2021-05-27 13:18:34 +0200

    Initial Commit

---
+++ Display.h
@@ -0,0 +1,11 @@
+/********************************************************************************
+ * Funktionen des Displays
+ * Nach der Initialisierung können verschiedene Parameter angezeigt werden
+********************************************************************************/
+void initDisplay();
+
+void showTemperature(float, boolean);
+void showHumidity(int hum, boolean);
+void showPressure(int, boolean);
+
+void showTime(String, boolean);
